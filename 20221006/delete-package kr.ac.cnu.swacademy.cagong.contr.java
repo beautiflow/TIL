442c0671489fb22a4d361deaecc1fe94d664b7e4
beautiflow <1114ny@naver.com>
2022-10-06 22:39:52 +0900

    20221006

---
+++ 20221006/delete-package kr.ac.cnu.swacademy.cagong.contr.java
@@ -0,0 +1,34 @@
+package kr.ac.cnu.swacademy.cagong.controller.api;
+
+import kr.ac.cnu.swacademy.cagong.dto.QuestionResponseDto;
+import kr.ac.cnu.swacademy.cagong.dto.QuestionSaveRequestDto;
+import kr.ac.cnu.swacademy.cagong.dto.QuestionUpdateRequestDto;
+import kr.ac.cnu.swacademy.cagong.service.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class QuestionApiController {
+
+    private final QuestionService questionService;
+
+    @PostMapping("/api/v1/question")
+    public Long save(@RequestBody QuestionSaveRequestDto requestDto){
+        return questionService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/question/{id}")
+    public Long update(@PathVariable Long id, @RequestBody QuestionUpdateRequestDto requestDto){
+        return questionService.update(id, requestDto);
+    }
+
+
+    @DeleteMapping("/api/v1/question/{id}")
+    public Long delete(@PathVariable Long id){
+        questionService.delete(id);
+        return id;
+    }
+}
+
+
